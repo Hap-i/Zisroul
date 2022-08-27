a4c2d426a7cdfd44c1d41add567dbcd84225f23c
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2022-08-27 10:10:27 +0530

    (feat): initial template added as static file

---
+++ packages/zisroul-data-service-gen/src/server/static/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`./.env.${process.env.STAGE}`],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return { uri: configService.get('DATABASE_URL') };
+      },
+    }),
+  ],
+})
+export class AppModule {}
---
+++ packages/zisroul-data-service-gen/src/server/static/src/main.ts
@@ -0,0 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3000;
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(port);
+}
+bootstrap();
