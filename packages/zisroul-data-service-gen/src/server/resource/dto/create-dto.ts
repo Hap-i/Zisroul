859d65b4959ab9421bbda46069f05a7b5e9bbcf5
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2022-08-30 23:32:44 +0530

    (feat): dto gen code

--- packages/zisroul-data-service-gen/src/server/resource/dto/create-dto.ts
+++ packages/zisroul-data-service-gen/src/server/resource/dto/create-dto.ts
@@ -1,7 +1,20 @@
-import { addImport } from 'util/ast';
-import { InputImport } from 'util/custom-types';
+import { keywordType } from './../../../util/ast-enums';
+import { astFilePrinter } from '../../../util/ast-printer';
+import {
+  addClass,
+  addImport,
+  addPropertyDeclaration,
+} from './../../../util/ast';
+import {
+  InputImport,
+  InputPropertyDeclaration,
+} from './../../../util/custom-types';
 
-export function createDto(importsInfo: InputImport[]) {
+export function createDto(
+  dtoName: string,
+  importsInfo: InputImport[],
+  propertyData: InputPropertyDeclaration[],
+) {
   const nodes = [];
   importsInfo.forEach((importInfo) => {
     nodes.push(
@@ -13,4 +26,37 @@ export function createDto(importsInfo: InputImport[]) {
     );
   });
   const members = [];
+  propertyData.forEach((property) => {
+    members.push(addPropertyDeclaration(property));
+  });
+  nodes.push(addClass(undefined, dtoName, members));
+  return nodes;
 }
+
+/* Example --
+
+const propertyData: InputPropertyDeclaration[] = [
+  {
+    name: 'title',
+    type: keywordType.STRING,
+    decorators: [
+      {
+        name: 'IsNotEmpty',
+        arguments: [],
+      },
+      {
+        name: 'ApiProperty',
+        arguments: [],
+      },
+    ],
+  },
+];
+const importsInfo = [
+  {
+    importItems: ['IsNotEmpty'],
+    importFrom: 'class-validator',
+    defaultImport: undefined,
+  },
+];
+const nodes = createDto('CreateTaskDto', importsInfo, propertyData);
+astFilePrinter(nodes); */
